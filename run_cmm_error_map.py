6042712899e063115f2435cae02602f6dcc68a7c
Eleanor Howick <elfnor@users.noreply.github.com>
2025-02-12 15:44:31 +1300

    nukita deploy file not found errors

---
+++ run_cmm_error_map.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from cmm_error_map.ls_cmm_error_map import main
+from cmm_error_map import __version__
+
+import tkinter
+
+print("Loading CMM Error Map GUI...")
+print(f"version {__version__}")
+# try:
+#     import pyi_splash
+
+#     pyi_splash.update_text("UI Loaded ...")
+#     pyi_splash.close()
+# except:
+#     pass
+
+print("Loading complete")
+main()
